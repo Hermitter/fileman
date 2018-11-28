8d02bd61c5c04943677395bc8dc8d4393daf00c7
Hermitter <chacin.carlos9798@gmail.com>
2018-11-27 20:47:16 -0500

    parameter name changes

--- fileUtility.go
+++ fileUtility.go
@@ -9,28 +9,28 @@ import (
 
 // Copy returns a File struct
 // from a specified file path.
-func Copy(path string) (File, error) {
+func Copy(filePath string) (File, error) {
 	// initialize empty File
 	var contents []byte
 	file := File{"", &contents}
 
 	// read & set file contents
-	contents, err := ioutil.ReadFile(path)
+	contents, err := ioutil.ReadFile(filePath)
 	if err != nil {
 		return file, err
 	}
 
 	// set file name from path
-	file.Name = filepath.Base(path)
+	file.Name = filepath.Base(filePath)
 
 	return file, nil
 }
 
 // Paste a file inside a specified path.
 // This will overwrite any file with the same name.
-func Paste(file File, path string, sync bool) error {
+func Paste(file File, filePath string, sync bool) error {
 	// create empty file
-	newFile, err := os.Create(path)
+	newFile, err := os.Create(filePath)
 	if err != nil {
 		return err
 	}
@@ -52,36 +52,36 @@ func Paste(file File, path string, sync bool) error {
 }
 
 // Delete will remove the specified file
-func Delete(path string) error {
-	return os.Remove(path)
+func Delete(filePath string) error {
+	return os.Remove(filePath)
 }
 
 // Cut will simultaneously Copy() & Delete()
 // a specified file
-func Cut(path string) (File, error) {
+func Cut(filePath string) (File, error) {
 	// copy specified file
-	file, err := Copy(path)
+	file, err := Copy(filePath)
 	if err != nil {
 		return file, err
 	}
 
 	// return copied file & any errors after deletion
-	return file, Delete(path)
+	return file, Delete(filePath)
 }
 
 // Rename a specified file
-func Rename(path, newName string) error {
+func Rename(filePath, newName string) error {
 	// extract directory from path
-	folderPath := filepath.Dir(path) + "/"
+	folderPath := filepath.Dir(filePath) + "/"
 
 	// return any errors from renaming file
-	return os.Rename(path, folderPath+newName)
+	return os.Rename(filePath, folderPath+newName)
 }
 
 // Move a file to a specified path
-func Move(path, newPath string) error {
+func Move(filePath, folderPath string) error {
 	// throw error if newPath doesn't exist || points to file
-	if pathType, err := os.Stat(newPath); os.IsNotExist(err) || pathType.Mode().IsRegular() {
+	if pathType, err := os.Stat(folderPath); os.IsNotExist(err) || pathType.Mode().IsRegular() {
 		// throw any path validating errors
 		if err != nil {
 			return err
@@ -91,15 +91,15 @@ func Move(path, newPath string) error {
 		return errors.New("You did not specify a folder")
 	}
 
-	// if newpath does not end with "/"
-	if newPath[len(newPath)-1:] != "/" {
+	// if folderPath does not end with "/"
+	if folderPath[len(folderPath)-1:] != "/" {
 		// add "/"
-		newPath = newPath + "/"
+		folderPath = folderPath + "/"
 	}
 
 	// extract file name from path
-	fileName := filepath.Base(path) + "/"
+	fileName := filepath.Base(filePath) + "/"
 
 	// return any errors from moving file
-	return os.Rename(path, newPath+fileName)
+	return os.Rename(filePath, folderPath+fileName)
 }

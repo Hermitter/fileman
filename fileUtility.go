1c2f8ea005d4eaa660c08ed7fa01d090e07b19c4
Hermitter <chacin.carlos9798@gmail.com>
2018-11-27 20:24:46 -0500

    Added file rename & move

--- fileUtility.go
+++ fileUtility.go
@@ -1,6 +1,7 @@
 package fileman
 
 import (
+	"errors"
 	"io/ioutil"
 	"os"
 	"path/filepath"
@@ -13,19 +14,19 @@ func Copy(path string) (File, error) {
 	var contents []byte
 	file := File{"", &contents}
 
-	// read file contents
+	// read & set file contents
 	contents, err := ioutil.ReadFile(path)
 	if err != nil {
 		return file, err
 	}
 
-	// save file name
+	// set file name from path
 	file.Name = filepath.Base(path)
 
 	return file, nil
 }
 
-// Paste creates a file inside a specified path.
+// Paste a file inside a specified path.
 // This will overwrite any file with the same name.
 func Paste(file File, path string, sync bool) error {
 	// create empty file
@@ -66,4 +67,39 @@ func Cut(path string) (File, error) {
 
 	// return copied file & any errors after deletion
 	return file, Delete(path)
+}
+
+// Rename a specified file
+func Rename(path, newName string) error {
+	// extract directory from path
+	folderPath := filepath.Dir(path) + "/"
+
+	// return any errors from renaming file
+	return os.Rename(path, folderPath+newName)
+}
+
+// Move a file to a specified path
+func Move(path, newPath string) error {
+	// throw error if newPath doesn't exist || points to file
+	if pathType, err := os.Stat(newPath); os.IsNotExist(err) || pathType.Mode().IsRegular() {
+		// throw any path validating errors
+		if err != nil {
+			return err
+		}
+
+		// Notify user of mistake
+		return errors.New("You did not specify a folder")
+	}
+
+	// if newpath does not end with "/"
+	if newPath[len(newPath)-1:] != "/" {
+		// add "/"
+		newPath = newPath + "/"
+	}
+
+	// extract file name from path
+	fileName := filepath.Base(path) + "/"
+
+	// return any errors from moving file
+	return os.Rename(path, newPath+fileName)
 }

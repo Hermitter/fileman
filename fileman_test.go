a5dcba34edfb1a4331f6aa4d2e9e5cfadd7c364c
Hermitter <chacin.carlos9798@gmail.com>
2019-01-03 17:06:59 -0500

    Edit test dir paths

--- fileman_test.go
+++ fileman_test.go
@@ -8,15 +8,15 @@ import (
 // TestMain creates a file for testing
 func TestMain(m *testing.M) {
 	// create test directories for File, Dir, Symlink
-	os.MkdirAll("fTest", os.ModePerm)
-	os.MkdirAll("dTest", os.ModePerm)
-	os.MkdirAll("sTest", os.ModePerm)
+	os.MkdirAll("./fTest", os.ModePerm)
+	os.MkdirAll("./dTest", os.ModePerm)
+	os.MkdirAll("./sTest", os.ModePerm)
 
 	// run fileman tests
 	code := m.Run()
 	// delete test directory & exit
-	os.RemoveAll("fTest")
-	os.RemoveAll("dTest")
-	os.RemoveAll("sTest")
+	os.RemoveAll("./fTest")
+	os.RemoveAll("./dTest")
+	os.RemoveAll("./sTest")
 	os.Exit(code)
 }

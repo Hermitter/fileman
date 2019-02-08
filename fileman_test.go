838333dbbbd2682f010ace2420d78804ab296af4
Hermitter <chacin.carlos9798@gmail.com>
2019-02-07 23:05:42 -0500

    GetType() test

--- fileman_test.go
+++ fileman_test.go
@@ -1,6 +1,7 @@
 package fileman
 
 import (
+	"io/ioutil"
 	"os"
 	"testing"
 )
@@ -11,6 +12,7 @@ func TestMain(m *testing.M) {
 	os.MkdirAll("./fTest", os.ModePerm)
 	os.MkdirAll("./dTest", os.ModePerm)
 	os.MkdirAll("./sTest", os.ModePerm)
+	os.MkdirAll("./fileman", os.ModePerm)
 
 	// run fileman tests
 	code := m.Run()
@@ -18,5 +20,35 @@ func TestMain(m *testing.M) {
 	os.RemoveAll("./fTest")
 	os.RemoveAll("./dTest")
 	os.RemoveAll("./sTest")
+	os.RemoveAll("./fileman")
 	os.Exit(code)
+}
+
+func TestGetType(t *testing.T) {
+	// create test items (file, dir, symLink)
+	ioutil.WriteFile("./fileman/file", []byte("Sup brah"), 0644)
+	err := os.Mkdir("./fileman/dir", os.ModePerm)
+	err = os.Symlink("./fileman/file", "./fileman/symLink")
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	// valid tests
+	if itemType, _ := GetType("./fileman/file", false); itemType != "file" || err != nil {
+		t.Error("GetType did not detect file")
+	}
+
+	if itemType, _ := GetType("./fileman/dir", false); itemType != "dir" || err != nil {
+		t.Error("GetType did not detect directory")
+	}
+
+	if itemType, _ := GetType("./fileman/symLink", false); itemType != "symLink" {
+		t.Error("GetType did not detect symLink")
+	}
+
+	// invalid test
+	if itemType, err := GetType("./fileman/fake", false); itemType != "" && err == nil {
+		t.Error("GetType did not fail correctly for non-Existing file")
+	}
 }
